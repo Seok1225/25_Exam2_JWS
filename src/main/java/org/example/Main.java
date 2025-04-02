4fd80619ebad164ec467cc8506466d1b8802f603
Seok1225 <koreait6642@gmail.com>
2025-04-02 15:20:55 +0900

    시험

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String cmd = sc.next();
-        System.out.println("== 명언 앱 실행 ==");
-        App.run(sc,cmd);
+        String cmd = sc.nextLine();
+        new MotivationApp(sc).run(sc,cmd);
+
+        sc.close();
     }
 }
--- src/main/java/org/example/MotivationApp.java
+++ src/main/java/org/example/MotivationApp.java
@@ -1,22 +1,24 @@
 package org.example;
 
 import java.util.Scanner;
+
 //- 등록 write , 목록 list , 상세보기 detail , 삭제 delete , 수정 modify 의 기능을 구현한다
-public class App {
+public class MotivationApp {
+    public MotivationApp(Scanner sc) {
+    }
 
     public static void run(Scanner sc, String cmd) {
         System.out.println("== 명언 앱 실행 ==");
-        System.out.println("명령어 ) ");
-        if(cmd.equals("종료"))
-        {
-            sc.close();
-        }
-        else if(cmd.equals("등록")) {
+        System.out.print("명령어 ) ");
+        if (cmd.equals("종료")) {
+            System.out.println("== 명언 앱 종료 ==");
+
+        } else if (cmd.equals("등록")) {
             System.out.println("명언 : ");
             sc.nextLine();
             System.out.println("작가: ");
             sc.nextLine();
-
+            cmd = sc.nextLine();
         } else if (cmd.equals("목록")) {
             System.out.println("번호    /    작가     /     명언");
 
